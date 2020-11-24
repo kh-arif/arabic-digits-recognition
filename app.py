6ae807e0edfd49a26c89984b01e02d2dcf1d46cc
kh-arif <khalid7380@hotmail.com>
2020-11-24 19:27:04 +0300

    modified app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("templates/index.html")
+    return render_template()
 
 @app.route('/save', methods=['POST'])
 def save():
