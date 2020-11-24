80664d7c42d0536e6238b12347b81b417ed530c7
kh-arif <khalid7380@hotmail.com>
2020-11-24 19:16:04 +0300

    try to mach path

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("arabic-digits-recognition.herokuapp.com/index.html")
+    return render_template("templates/index.html")
 
 @app.route('/save', methods=['POST'])
 def save():
